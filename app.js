e66b2e7da3cfc0a5a89c0ae8efaa8522f187e6e1
jerry11671 <jegoisgs66@gmail.com>
2025-02-01 15:20:04 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express(); 
+const port = 5000;
+const cors = require('cors');
+
+app.use(cors());
+
+app.get('/api/task0/retrieve', (req, res) => {
+    res.status(200).json({
+        email: 'jerrygodson3@gmail.com',
+        current_datetime: new Date().toISOString(),
+        github_url: 'https://github.com/jerry11671/publicAPI.git'
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+})
